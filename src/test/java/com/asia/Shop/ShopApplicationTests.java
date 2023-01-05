d1889f8bd47826849b4b4416be68db16d56c7ee4
Joanna Szymczak <jszymczak@xtm-intl.com>
2023-01-05 17:15:31 +0100

    initial commit

---
+++ src/test/java/com/asia/Shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asia.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
